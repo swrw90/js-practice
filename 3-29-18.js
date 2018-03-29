82edd22cb093b90aab1f816c129f626f6dd20703
Seth Watson <seth.watson1@gmail.com>
2018-03-29 15:00:33 -0700

    initial commit

---
+++ 3-29-18.js
@@ -0,0 +1,8 @@
+// Write a function that converts C to F.
+// Function will return a value instead of output to console.
+// F = 1.8 * C + 32
+
+const celciusToFarenheit = num => (1.8 * num) + 32;
+console.log(celciusToFarenheit(100));
+
+
