b1595076d362b088fb2efbd1cc6e5353438f517c
Seth Watson <seth.watson1@gmail.com>
2018-03-29 15:18:32 -0700

    added bool problem to find minimumCarCount

--- 3-29-18.js
+++ 3-29-18.js
@@ -1,8 +1,14 @@
-// Write a function that converts C to F.
+//1. Write a function that converts C to F.
 // Function will return a value instead of output to console.
 // F = 1.8 * C + 32
 
 const celciusToFarenheit = num => (1.8 * num) + 32;
-console.log(celciusToFarenheit(100));
+// console.log(celciusToFarenheit(100));
 
 
+//2. Write a function that checks if text is longer than 7 Characters.
+// Function will take a string as a parameter and returns a Boolean.
+
+let minimumCharCount = 7;
+const isValidLength = text => text.length > minimumCharCount;
+console.log(isValidLength("pooped the bed"));
