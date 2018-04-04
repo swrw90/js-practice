114c42af04de085ec4d44c9c128dbaf90bfa03b9
Seth Watson <seth.watson1@gmail.com>
2018-04-03 22:09:33 -0700

    created fucktorial recursive function

--- 4-3-18.js
+++ 4-3-18.js
@@ -1,4 +1,4 @@
-// Factorial 
+// 1. Factorial 
 // 5! = 5 x 4 x 3 x 2 x 1
 // 5! = 120 
 
@@ -16,3 +16,27 @@ let factorial = (num) => {
 };
 
 // console.log(factorial(7));
+
+//2. Recursive Factorial.
+
+let fucktorial = (number) => {
+    if (number < 0) {
+        return -1;
+    } else if (number == 0) {
+        return 1;
+    } else {
+        return number * fucktorial(number - 1);
+    }
+}
+
+console.log(fucktorial(50));
+
+// CallStack 
+//     - fucktorial(7) 5040 
+//         - fuctorial(6) 720
+//             - fucktorial(5) 120
+//                 - fucktorial(4) 24
+//                     - fucktorial(3) 6
+//                         - fucktorial(2) 2 
+//                             - fucktorial(1) 1 
+//                                 - fucktorial(0) 1 
