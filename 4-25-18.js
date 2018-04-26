ec29bf56cf94f1515226a5b2063527f54a6cde87
Seth Watson <seth.watson1@gmail.com>
2018-04-25 23:58:29 -0700

    added practice problems

---
+++ 4-25-18.js
@@ -0,0 +1,40 @@
+// 1. Have the function QuestionsMarks( ) take the str string parameter, 
+// which will contain single digit numbers, letters, and question marks, 
+// and check if there are exactly 3 question marks between every pair of two numbers that add up to 10. 
+// If so, then your program should return the string true, otherwise it should return the string false. 
+// If there aren't any two numbers that add up to 10 in the string, then your program should return false as well. 
+
+// For example: if str is "arrb6???4xxbl5???eee5" then your program should return true because there are exactly 3 
+// question marks between 6 and 4, and 3 question marks between 5 and 5 at the end of the string. 
+
+function QuestionsMarks(str) { 
+
+  return str; 
+         
+}
+
+QuestionsMarks(readline());     
+
+
+// 2. Using the JavaScript language, have the function VowelSquare( ) take the strArr parameter being passed which will be a 2D matrix of some arbitrary size 
+// filled with letters from the alphabet, and determine if a 2x2 square composed entirely of vowels exists in the matrix. 
+// For example: strArr is ["abcd", "eikr", "oufj"] then this matrix looks like the following: 
+
+// a b c d 
+// e i k r
+// f j 
+
+// Within this matrix there is a 2x2 square of vowels starting in the second row and first column, 
+// namely, ei, ou. If a 2x2 square of vowels is found your program should return the top-left position (row-column) of the square, 
+// so for this example your program should return 1-0. If no 2x2 square of vowels exists, then return the string not found . 
+// If there are multiple squares of vowels, return the one that is at the most top-left position in the whole matrix. The input matrix will at least be of size 2x2. 
+
+function VowelSquare(strArr) { 
+
+  // code goes here  
+  return strArr; 
+         
+}
+   
+VowelSquare(readline());                            
+
